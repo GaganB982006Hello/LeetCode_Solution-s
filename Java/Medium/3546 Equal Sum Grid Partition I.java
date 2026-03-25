d0c8dcf8844ae511417fabfd8deee4f7cb2407aa
Gagan B <gaganb982006@gmail.com>
2026-03-25 17:38:35 +0530

    Create 3546 Equal Sum Grid Partition I.java

---
+++ Java/Medium/3546 Equal Sum Grid Partition I.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean canPartitionGrid(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        long totalSum = 0;
+
+        for (int[] row : grid) {
+            for (int val : row) {
+                totalSum += val;
+            }
+        }
+
+        if (totalSum % 2 != 0) return false;
+        long target = totalSum / 2;
+
+        // Check Horizontal Cuts
+        long rowSum = 0;
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = 0; j < n; j++) {
+                rowSum += grid[i][j];
+            }
+            if (rowSum == target) return true;
+        }
+
+        // Check Vertical Cuts
+        long colSum = 0;
+        for (int j = 0; j < n - 1; j++) {
+            for (int i = 0; i < m; i++) {
+                colSum += grid[i][j];
+            }
+            if (colSum == target) return true;
+        }
+
+        return false;
+    }
+}
